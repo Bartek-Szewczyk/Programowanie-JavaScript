2c1845b63153f3308b4971af305bb4ee29252bf3
Bartek-Szewczyk <ba20001000@gmail.com>
2020-10-09 13:29:25 +0200

    day2

--- c1 lightbox/main.js
+++ c1 lightbox/main.js
@@ -18,16 +18,41 @@ for (let index = 0; index < imgs.length; index++) {
     const img = imgs[index];
     console.log(img);
     img.addEventListener('click', showLightbox)
-
 }
+
+const lightbox = document.querySelector('.lightbox');
+
+
+
+let NextEl;
+let imgUrl;
+let img;
 
 function showLightbox(ev) {
     const prevEl = ev.target.prevElementSibling;
-    const NextEl = ev.target.nextElementSibling;
-    const lightbox = document.querySelector('.lightbox');
-    const img = document.querySelector('.lightbox img');
-    const imgUrl = ev.target.src;
+    NextEl = ev.target.nextElementSibling.currentSrc;
+
+    img = document.querySelector('.lightbox img');
+    console.log(NextEl)
+    imgUrl = ev.target.src;
     img.src = imgUrl;
     lightbox.classList.add('visible');
 
+    const prev = document.querySelector('.next');
+    prev.addEventListener('click', showNext);
+
+    function showNext(ev) {
+        img = NextEl;
+
+
+        console.log(NextEl);
+    }
+
 }
+
+// const background = document.querySelector('.lightbox')
+// background.addEventListener('click', closeBackground)
+
+// function closeBackground() {
+//     lightbox.classList.remove('visible');
+// }
