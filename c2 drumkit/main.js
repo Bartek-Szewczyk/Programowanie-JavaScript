81ae2c8b764cabf2606c61749b638f87e4579618
Bartek-Szewczyk <ba20001000@gmail.com>
2020-10-16 08:56:40 +0200

    add sounds

--- c2 drumkit/main.js
+++ c2 drumkit/main.js
@@ -7,21 +7,41 @@ function onKeyPress(ev) {
     let sound;
     let soundName;
     switch (ev.code) {
-        case 'KeyA':
-            soundName = 'boom';
-            sound = document.querySelector('#boom');
+        case "KeyA":
+            soundName = "boom";
+            sound = document.querySelector("#boom");
             break;
-        case 'KeyS':
-            soundName = 'clap';
-            sound = document.querySelector('#clap');
+        case "KeyS":
+            soundName = "clap";
+            sound = document.querySelector("#clap");
             break;
-        case 'KeyD':
-            soundName = 'hihat';
-            sound = document.querySelector('#hihat');
+        case "KeyD":
+            soundName = "hihat";
+            sound = document.querySelector("#hihat");
             break;
-        case 'KeyF':
-            soundName = 'kick';
-            sound = document.querySelector('#kick');
+        case "KeyF":
+            soundName = "kick";
+            sound = document.querySelector("#kick");
+            break;
+        case "KeyG":
+            soundName = "openhat";
+            sound = document.querySelector("#openhat");
+            break;
+        case "KeyH":
+            soundName = "ride";
+            sound = document.querySelector("#ride");
+            break;
+        case "KeyJ":
+            soundName = "snare";
+            sound = document.querySelector("#snare");
+            break;
+        case "KeyK":
+            soundName = "tink";
+            sound = document.querySelector("#tink");
+            break;
+        case "KeyL":
+            soundName = "tom";
+            sound = document.querySelector("#tom");
             break;
     }
 
