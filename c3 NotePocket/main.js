f4c9e8248dfc5ff226f6b6c11d2fe113282e7a32
Bartek-Szewczyk <ba20001000@gmail.com>
2020-11-01 21:28:10 +0100

    add title and content to note

--- c3 NotePocket/main.js
+++ c3 NotePocket/main.js
@@ -3,80 +3,80 @@ let notes = [];
 
 document.querySelector('#noteAdd').addEventListener('click', onNewNote)
 
+
+
 function onNewNote() {
     const title = document.querySelector('#noteTitle').value;
     const content = document.querySelector('#noteContent').value;
     console.log(title, content);
+
+    // nowa notatka
+    const note = {
+        title: title,
+        content: content,
+        colour: '#ff000',
+        pinned: false,
+        createDate: new Date()
+    };
+    //notatka dodana do ablicy notatek
+    notes.push(note);
+
+
+    //tablioca dodana do localstorage
+    localStorage.setItem(localStorageNoteKey, JSON.stringify(notes));
+
+    //odczytanie tablicy notatek 
+    const notesFromStorage = JSON.parse(localStorage.getItem(localStorageNoteKey));
+    notes = notesFromStorage.map(note => {
+        note.createDate = new Date(note.createDate);
+        return note;
+    })
+
+    addHtml();
 }
 
-// nowa notatka
-const note = {
-    title: 'nowa notatka',
-    content: 'treść notatki',
-    colour: '#ff000',
-    pinned: false,
-    createDate: new Date()
-};
-//notatka dodana do ablicy notatek
-notes.push(note);
-notes.push(note);
 
-for (let note of notes) {
 
-    const htmlNote = ` 
-    <section class = "note">
-    <h1>${note.title}</h1>
-    <p>${note.content}</p>
-    <h4>${note.createDate.toLocaleString()}</h4>
-    </section>
-    `;
+// // zmiana html z poziomu js sposob brutalny 
+// for (let note of notes) {
 
-    document.body.innerHTML += htmlNote;
+//     const htmlNote = ` 
+//     <section class = "note">
+//     <h1>${note.title}</h1>
+//     <p>${note.content}</p>
+//     <h4>${note.createDate.toLocaleString()}</h4>
+//     </section>
+//     `;
 
-}
-//tablioca dodana do localstorage
-localStorage.setItem(localStorageNoteKey, JSON.stringify(notes));
+//     const main = document.querySelector('main');
+//     main.innerHTML += htmlNote;
 
-//odczytanie tablicy notatek 
-const notesFromStorage = JSON.parse(localStorage.getItem(localStorageNoteKey));
-notes = notesFromStorage.map(note => {
-    note.createDate = new Date(note.createDate);
-    return note;
-})
+// }
 
-// zmiana html z poziomu js sposob brutalny 
-for (let note of notes) {
-
-    const htmlNote = ` 
-    <section class = "note">
-    <h1>${note.title}</h1>
-    <p>${note.content}</p>
-    <h4>${note.createDate.toLocaleString()}</h4>
-    </section>
-    `;
+function addHtml() {
 
     const main = document.querySelector('main');
-    main.innerHTML += htmlNote;
+    main.innerHTML = '';
+    // zmiana html-a z poziomu js-a - sposób obiektowy
+    for (let note of notes) {
 
-}
+        const htmlSection = document.createElement('section');
+        const htmlTitle = document.createElement('h1');
+        const htmlContent = document.createElement('p');
+        const htmlDate = document.createElement('h4');
+
+        htmlSection.classList.add('note');
+        htmlTitle.innerHTML = note.title;
+        htmlContent.innerHTML = note.content;
+        htmlDate.innerHTML = note.createDate.toLocaleString();
+
+        htmlSection.appendChild(htmlTitle);
+        htmlSection.appendChild(htmlContent);
+        htmlSection.appendChild(htmlDate);
 
 
-// zmiana html-a z poziomu js-a - sposób obiektowy
-for (let note of notes) {
-    const htmlSection = document.createElement('section');
-    const htmlTitle = document.createElement('h1');
-    const htmlContent = document.createElement('p');
-    const htmlDate = document.createElement('h4');
+        main.appendChild(htmlSection);
+    }
 
-    htmlSection.classList.add('note');
-    htmlTitle.innerHTML = note.title;
-    htmlContent.innerHTML = note.content;
-    htmlDate.innerHTML = note.createDate.toLocaleString();
 
-    htmlSection.appendChild(htmlTitle);
-    htmlSection.appendChild(htmlContent);
-    htmlSection.appendChild(htmlDate);
-
-    const main = document.querySelector('main');
-    main.appendChild(htmlSection);
 }
