ccb6c79a1151d1ae04812ecc42652125e635b6ec
Bartek-Szewczyk <ba20001000@gmail.com>
2020-12-28 17:29:17 +0100

    create class snow

--- c6 Snow Animation/snow.js
+++ c6 Snow Animation/snow.js
@@ -1,20 +1,3 @@
 class Snow {
-    constructor(flakesCount) {
-        new Flakes(flakesCount)
-        this.flakeCount = flakesCount;
-        this.flakes = [];
-        this.mX = -100;
-        this.mY = -100;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    }
 
-    PlaySnow() {
-        addFlakes();
-        for (let i = 0; i < this.flakesCount; i++) {
-
-
-        }
-
-    }
 }
