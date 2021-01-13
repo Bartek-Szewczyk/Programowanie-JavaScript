0d6e664688f83fd220c8eceededdba739aff37d9
Bartek-Szewczyk <ba20001000@gmail.com>
2021-01-13 18:30:30 +0100

    4 channel

--- c2 drumkit/main.js
+++ c2 drumkit/main.js
@@ -1,11 +1,51 @@
 document.body.addEventListener('keypress', onKeyPress)
-document.querySelector('#playbtn').addEventListener('click', playChannel);
+const channel = [];
+const channel2 = [];
+const channel3 = [];
+const channel4 = [];
+let rec;
+document.querySelector('#playbtn').addEventListener('click', () => {
+    playChannel(channel)
+});
+document.querySelector('#playbtn2').addEventListener('click', () => {
+    playChannel(channel2)
+});
+document.querySelector('#playbtn3').addEventListener('click', () => {
+    playChannel(channel3)
+});
+document.querySelector('#playbtn4').addEventListener('click', () => {
+    playChannel(channel4)
+});
+document.querySelector('#playAllbtn').addEventListener('click', () => {
+
+    console.log(channel);
+    console.log(channel2);
+    console.log(channel3);
+    console.log(channel4);
+    const allChannel = channel.concat(channel2, channel3, channel4);
+    console.log(allChannel);
+    playChannel(allChannel);
+})
 let recordStart;
+let recordStart2;
 console.log(recordStart);
 document.querySelector('#recordbtn').addEventListener('click', () => {
     recordStart = Date.now();
+    rec = 1;
 })
-const channel = [];
+document.querySelector('#recordbtn2').addEventListener('click', () => {
+    recordStart2 = Date.now();
+    rec = 2;
+})
+document.querySelector('#recordbtn3').addEventListener('click', () => {
+    recordStart3 = Date.now();
+    rec = 3;
+})
+document.querySelector('#recordbtn4').addEventListener('click', () => {
+    recordStart4 = Date.now();
+    rec = 4;
+})
+
 
 
 
@@ -53,13 +93,44 @@ function onKeyPress(ev) {
             break;
     }
 
-    if (sound && recordStart != undefined) {
+    if (sound && rec == 1) {
         const keyPressTime = Date.now() - recordStart;
         const recordedSound = {
             sound: soundName,
             time: keyPressTime,
         }
         channel.push(recordedSound);
+
+        sound.play();
+        drum(soundName)
+    }
+    if (sound && rec == 2) {
+        const keyPressTime = Date.now() - recordStart2;
+        const recordedSound = {
+            sound: soundName,
+            time: keyPressTime,
+        }
+        channel2.push(recordedSound);
+        sound.play();
+        drum(soundName)
+    }
+    if (sound && rec == 3) {
+        const keyPressTime = Date.now() - recordStart3;
+        const recordedSound = {
+            sound: soundName,
+            time: keyPressTime,
+        }
+        channel3.push(recordedSound);
+        sound.play();
+        drum(soundName)
+    }
+    if (sound && rec == 4) {
+        const keyPressTime = Date.now() - recordStart4;
+        const recordedSound = {
+            sound: soundName,
+            time: keyPressTime,
+        }
+        channel4.push(recordedSound);
         sound.play();
         drum(soundName)
     } else if (sound) {
@@ -68,7 +139,7 @@ function onKeyPress(ev) {
     }
 }
 
-function playChannel() {
+function playChannel(channel) {
     for (let index = 0; index < channel.length; index++) {
         const soundObject = channel[index];
 
